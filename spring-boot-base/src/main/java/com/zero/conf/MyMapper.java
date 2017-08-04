d44bd1f8f97d06521941b13970d93253a96b05b4
yezhaoxing <1041165785@qq.com>
2017-08-04 10:54:52 +0800

    测试用户名

--- spring-boot-base/src/main/java/com/zero/conf/MyMapper.java
+++ spring-boot-base/src/main/java/com/zero/conf/MyMapper.java
@@ -4,7 +4,7 @@ import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
- * 继承自己的MyMapper<br/>
+ * 继承自己的MyMapper  <br/>
  * FIXME 特别注意，该接口不能被扫描到，否则会出错
  *
  * @author yezhaoxing
