4310a7bd7d1cdd473062a9a55446aecf4d28a6a2
yezhaoxing <1041165785@qq.com>
2017-09-28 15:04:39 +0800

    code clean

--- spring-boot-base/src/main/java/com/zero/constant/PointConstant.java
+++ spring-boot-base/src/main/java/com/zero/constant/PointConstant.java
@@ -7,8 +7,12 @@ package com.zero.constant;
 public interface PointConstant {
 
     int POINT_LOGIN = 2;
+
     int POINT_CHECK = 2;
+
     int POINT_CONTINUE_3 = 5;
+
     int POINT_CONTINUE_7 = 30;
+
     int POINT_CONTINUE_30 = 200;
 }
