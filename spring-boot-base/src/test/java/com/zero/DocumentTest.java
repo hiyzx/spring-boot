bbb9d9981bbb2cb1aa37dc8d771426cc90774316
yezhaoxing <1041165785@qq.com>
2017-11-12 11:57:29 +0800

    百度doc调试

--- spring-boot-base/src/test/java/com/zero/DocumentTest.java
+++ spring-boot-base/src/test/java/com/zero/DocumentTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.io.File;
 
 /**
@@ -16,15 +17,23 @@ import java.io.File;
 @SpringBootTest
 public class DocumentTest {
 
+    @Resource
+    private DocumentUtil documentUtil;
+
     @Test
     public void testUpload() {
         File file = new File("E:\\《亿级流量电商详情页系统实战（第二版）：缓存架构+高可用服务架构+微服务架构》.doc");
         String title = "亿级流量电商详情页系统实战";
-        DocumentUtil.createDocument(file, title, "doc", null, null, null);
+        documentUtil.createDocument(file, title, "doc", null, null, null);
     }
 
     @Test
     public void testQuery() {
-        DocumentUtil.getDocument("doc-hk9je0dax4pvf6n");
+        documentUtil.getDocument("doc-hk9je0dax4pvf6n");
+    }
+
+    @Test
+    public void testGetDownLoadUrl() {
+        documentUtil.getDownloadUrl("doc-hkbg5v126n5fq8q");
     }
 }
