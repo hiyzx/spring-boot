c37ed28d56ab4cd56aee6e3167eded0c1f368295
叶招兴 <[452002276@qq.com]>
2019-07-05 14:29:53 +0800

    上传

---
+++ job/xxl-job/src/main/java/org/zero/xxl/job/XxlJobApplication.java
@@ -0,0 +1,12 @@
+package org.zero.xxl.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApplication.class, args);
+    }
+}
