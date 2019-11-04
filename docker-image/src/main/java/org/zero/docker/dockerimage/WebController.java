3ef1e862848d8ebccea72ce94bd34ac3fd441618
hiyzx <hiyzx>
2019-11-04 21:26:44 +0800

    添加docker镜像制作

---
+++ docker-image/src/main/java/org/zero/docker/dockerimage/WebController.java
@@ -0,0 +1,23 @@
+package org.zero.docker.dockerimage;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yezhaoxing
+ * @date 2019/11/2
+ */
+@RestController
+public class WebController {
+
+	@GetMapping("/hello")
+	public Map<String,String> hello(){
+		Map<String, String> map = new HashMap<>();
+		map.put("name", "hiyzx");
+		map.put("age", "1");
+		return map;
+	}
+}
