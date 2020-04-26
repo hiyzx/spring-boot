7147a58200ccbfe77e90ab7272f1f7f1ac46fb7f
shuihan <452002276@qq.com>
2020-04-26 10:05:55 +0800

    添加grpc

---
+++ rpc/grpc/grpc-client/src/main/java/org/zero/grpc/client/GrpcClientApplication.java
@@ -0,0 +1,16 @@
+package org.zero.grpc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 水寒
+ * @date 2020/4/26
+ */
+@SpringBootApplication
+public class GrpcClientApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(GrpcClientApplication.class, args);
+	}
+}
--- rpc/grpc/grpc-server/src/main/java/org/zero/grpc/server/HelloServer.java
+++ rpc/grpc/grpc-server/src/main/java/org/zero/grpc/server/HelloServer.java
@@ -2,22 +2,23 @@ package org.zero.grpc.server;
 
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.zero.grpc.api.HelloServiceGrpc;
-import org.zero.grpc.api.HelloServiceProto;
+import org.zero.grpc.api.HelloReply;
+import org.zero.grpc.api.HelloRequest;
+import org.zero.grpc.api.MyServiceGrpc;
 
 /**
  * @author 水寒
  * @date 2020/4/24
  */
 @GrpcService
-public class HelloServer extends HelloServiceGrpc.HelloServiceImplBase {
+public class HelloServer extends MyServiceGrpc.MyServiceImplBase {
 
     @Override
-    public void sayHello(HelloServiceProto.HelloRequest request, StreamObserver<HelloServiceProto.HelloResponse> responseObserver) {
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
         String name = request.getName();
         System.out.println("接收请求,name=" + name);
-	    HelloServiceProto.HelloResponse response = HelloServiceProto.HelloResponse.newBuilder().setReply("hello " + name).build();
-	    responseObserver.onNext(response);
-	    responseObserver.onCompleted();
+        HelloReply response = HelloReply.newBuilder().setMessage("hello " + name).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
